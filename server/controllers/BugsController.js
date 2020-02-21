0efcf946621b422c21dab84d6e06ec1cbfe59464
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-02-21 11:57:11 -0700

    started BugsController

---
+++ server/controllers/BugsController.js
@@ -0,0 +1,59 @@
+import express from "express";
+import bugService from "../services/BugService";
+
+export default class BugsController {
+  constructor() {
+    this.router = express
+      .Router()
+      .get("", this.getAll)
+      .get("/:id", this.getById)
+      .post("", this.create)
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let data = await bugService.getAll();
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      let data = await bugService.getById(req.params.id);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      let data = await bugService.create(req.body);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      let data = await bugService.edit(req.params.id, req.body);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      let data = await bugService.getAll(req.params.id);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
